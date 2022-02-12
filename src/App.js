1dd51658668a4d887b8dd34c4ad54a0c87276aa0
bad89 <badeailham@gmail.com>
2022-02-12 09:49:37 +0700

    changetextchatwidget

--- src/App.js
+++ src/App.js
@@ -5,33 +5,29 @@ import "./App.css";
 import Home from "./components/pages/Home";
 import MessengerCustomerChat from "react-messenger-customer-chat";
 
-
-
 function App() {
   return (
-    
-    
-      <Router>
-        <Navbar />
+    <Router>
+      <Navbar />
 
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-        
-        
-        <MessengerCustomerChat pageId="101589709123007" appId="481482256871485" />
-        <MessengerCustomerChat pageId="114516117825494" appId="481482256871485" />
-        <MessengerCustomerChat pageId="103549992260062" appId="481482256871485" />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
 
-      </Router>
+      <MessengerCustomerChat pageId="101589709123007" appId="481482256871485" />
 
-      
-    
+      <MessengerCustomerChat pageId="114516117825494" appId="481482256871485" />
 
-    
+      <div className="fb_dialog_content">
+        <div className="fb_dialog_mobile">
+          <MessengerCustomerChat
+            pageId="103549992260062"
+            appId="481482256871485"
+          />
+        </div>
+      </div>
+    </Router>
   );
 }
-
-
 
 export default App;
